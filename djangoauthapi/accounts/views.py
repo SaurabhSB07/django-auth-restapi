d6fb3e1640a0f8237ff7110b9e35c3b41f67ff05
SaurabhSB07 <barkesaurabh07@gmail.com>
2025-07-02 23:05:02 +0530

    Custom User Login

--- djangoauthapi/accounts/views.py
+++ djangoauthapi/accounts/views.py
@@ -1,6 +1,8 @@
 from rest_framework.response import Response  
 from rest_framework.views import APIView
-from .serializers import UserSerializer
+from .serializers import UserSerializer,UserLoginSerializer
+from django.contrib.auth import authenticate
+
 
 class UserRegistrationView(APIView):
     def post(self, request, format=None):
@@ -9,3 +11,18 @@ class UserRegistrationView(APIView):
             serializer.save()
             return Response({'msg': 'Registration successful'})
         return Response(serializer.errors, status=400)
+    
+class UserLoginView(APIView):
+    def post(self, request, format=None):
+        serializer = UserLoginSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            email = serializer.validated_data.get("email")
+            password = serializer.validated_data.get("password")
+
+            user = authenticate(email=email, password=password)  
+            if user is not None:
+                return Response({'msg': 'Login successful'})
+            else:
+                return Response({
+                    "errors": {"non_field_errors": ["Email or password is incorrect"]}
+                }, status=400)
