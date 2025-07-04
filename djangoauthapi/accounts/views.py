fc6964bc6b6db1a705204b03b75297889928a5d8
SaurabhSB07 <barkesaurabh07@gmail.com>
2025-07-04 19:25:37 +0530

    some left setups in views

--- djangoauthapi/accounts/views.py
+++ djangoauthapi/accounts/views.py
@@ -80,4 +80,4 @@ class UserPasswordResetView(APIView):
   def post(self, request, uid, token, format=None):
     serializer = UserPasswordResetSerializer(data=request.data, context={'uid':uid, 'token':token})
     serializer.is_valid(raise_exception=True)
-    return Response({'msg':'Password Reset Successfully'}, status=status.HTTP_200_OK)    
+    return Response({'msg':'Password Reset Successfully'}, status=200)    
