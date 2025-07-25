e2a188999cebff9c8a8417dbb5154a1d4d818c4f
Saurabh Barke <barkesaurabh07@gmail.com>
2025-07-25 21:38:57 +0530

    Update urls.py

--- djangoauthapi/accounts/urls.py
+++ djangoauthapi/accounts/urls.py
@@ -1,14 +1,19 @@
-from django.urls import URLPattern,path
-from .views import UserLoginView,UserRegistrationView,UserProfileView
-from .views import UserChagePasswordResetEmailView,UserChangePasswordView,UserPasswordResetView
-
-
+from django.urls import path
+from .views import (
+    UserLoginView,
+    UserRegistrationView,
+    UserProfileView,
+    UserChagePasswordResetEmailView,
+    UserChangePasswordView,
+    UserPasswordResetView,
+    
+)
 
 urlpatterns = [
-    path('register/',UserRegistrationView.as_view(),name='regester'),
-    path('login/',UserLoginView.as_view(),name='login'),
-    path('profile/',UserProfileView.as_view(),name='profile'),
-    path('changepassword/',UserChangePasswordView.as_view(),name='reset-password'),
-    path('user-reset-pass-email/',UserChagePasswordResetEmailView.as_view(),name='user-reset-pass-email'),
-    path('reset-password/<uid>/<token>/', UserPasswordResetView.as_view(), name='reset-password')
+    path('register/', UserRegistrationView.as_view(), name='register'),  
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
+    path('changepassword/', UserChangePasswordView.as_view(), name='change-password'),
+    path('user-reset-pass-email/', UserChagePasswordResetEmailView.as_view(), name='user-reset-pass-email'),
+    path('reset-password/<uid>/<token>/', UserPasswordResetView.as_view(), name='reset-password'),
 ]
