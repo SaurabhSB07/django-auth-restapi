265ee9a99ea624b9d5371d3475bc779837628640
Saurabh Barke <barkesaurabh07@gmail.com>
2025-07-25 21:39:49 +0530

    Update utils.py

--- djangoauthapi/accounts/utils.py
+++ djangoauthapi/accounts/utils.py
@@ -2,12 +2,16 @@ from django.core.mail import EmailMessage
 import os
 
 class Util:
-  @staticmethod
-  def send_email(data):
-    email = EmailMessage(
-      subject=data['subject'],
-      body=data['body'],
-      from_email=os.environ.get('EMAIL_FROM'),
-      to=[data['to_email']]
-    )
-    email.send()
+    @staticmethod
+    def send_email(data):
+        try:
+            email = EmailMessage(
+                subject=data['subject'],
+                body=data['body'],
+                from_email=os.environ.get('EMAIL_FROM'),
+                to=[data['to_email']]
+            )
+            email.send(fail_silently=False)
+        except Exception as e:
+            print(f"Failed to send email: {e}")
+            raise
